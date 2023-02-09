2a820afc35354a8eb85779be188ca2c30fa63091
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2023-02-10 03:43:42 +0530

    first commit

---
+++ flask_hello/app.py
@@ -0,0 +1,38 @@
+# import string
+from flask import Flask,render_template,request,jsonify
+import pickle
+import pandas as pd
+import numpy as np
+# from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+app = Flask(__name__)
+pipeline_ls = pickle.load(open('phishing.pkl','rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    Url_get = request.form.get('URL')
+    print(Url_get)
+    
+    result = pipeline_ls.predict([[Url_get]])
+
+    return "this result is {}".fmt(result)
+    print(result)
+    
+    # if result == 'Bad':
+    #     # return jsonify({'label':1})
+    #     # return render_template('index.html',label=1)
+    #     return "This is not a phising website"
+    # else:
+    #     # return jsonify({'label':-1})
+    #     # return render_template('index.html',label=-1)
+    #     return "This is a phising website"
+    # return "The Mintemp is {} , Maxtemp is{} and sunshine is{}".format(mintemp,maxtemp,sunshine)
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=False, port=8001)
